23f1a4ec46bf22c976f5d0a20ac6b149ad34730b
Mohamed <hirole.mohamed@gmail.com>
2018-07-23 19:27:23 -0400

    Added Google Maps opening script

---
+++ mapIt.py
@@ -0,0 +1,14 @@
+#! python3
+#mapIt.py
+
+import webbrowser, sys
+if len(sys.argv) > 1:
+    #get address from command line
+    address = ' '.join(sys.argv[1:])
+
+# TODO: Get address from clipboard
+
+else:
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
